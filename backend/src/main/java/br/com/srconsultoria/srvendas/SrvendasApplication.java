29e6623535c9b0c1188f068e402d99fcf4b31504
Saulus Ribeiro <saulusribeiro@gmail.com>
2021-09-07 10:40:47 -0300

    Project Created

---
+++ backend/src/main/java/br/com/srconsultoria/srvendas/SrvendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.srconsultoria.srvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvendasApplication.class, args);
+	}
+
+}
