e584e0fa5b56dc316cf9bf31062f65f05d1f0ac9
Saulus Ribeiro <saulusribeiro@gmail.com>
2021-09-08 09:45:03 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/saulus-ribeiro-49849b35/" target="_blank" rel="noreferrer">Saulus Ribeiro</a></p>
+        <p className="text-light"><small><strong>SRVendas Spring React</strong><br />
+          Sistema de Vendas desenvolvido pela SR Consultoria e Projetos: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+
+  );
+}
+
+export default Footer;
