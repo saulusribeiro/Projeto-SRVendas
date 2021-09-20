b8e0c282e40e0d83ddfb90f9305f1a63e544fa25
Saulus Ribeiro <saulusribeiro@gmail.com>
2021-09-20 14:39:58 -0300

    Bar Chart Integration

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,3 +1,6 @@
+
+// Função para arredondar números 
+
 export const round = (value: number, precision: number) => {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier; }
